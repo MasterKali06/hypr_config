6d1f26f85aaa209007ae0cf0e3930e2fa99f4e2c
master <mohammadmasterr@gmail.com>
2025-10-17 19:04:13 +0330

    hypr simple config

---
+++ fish/config.fish
@@ -0,0 +1,21 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# --- Android SDK setup ---
+set -x ANDROID_HOME $HOME/Android/Sdk
+set -x ANDROID_SDK_ROOT $HOME/Android/Sdk
+set -x PATH $PATH $ANDROID_HOME/emulator $ANDROID_HOME/platform-tools $ANDROID_HOME/tools $ANDROID_HOME/tools/bin
+
+# --- Java setup (optional, if needed) ---
+# If using OpenJDK from Arch:
+set -x JAVA_HOME /usr/lib/jvm/java-17-openjdk
+
+# Enable bracketed paste
+function enable_bracketed_paste --on-event fish_prompt
+    printf '\e[?2004h'
+end
+
+function disable_bracketed_paste --on-event fish_preexec
+    printf '\e[?2004l'
+end
